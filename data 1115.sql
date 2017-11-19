59a478c3134c1eff97345a2b33844e60e8823e85
jhu70 <jhu70@bonny.smith.edu>
2017-11-18 23:35:34 -0500

    New data 1115

--- data 1115.sql
+++ data 1115.sql
@@ -1,4 +1,4 @@
-SELECT info_type_id, movie_info.movie_id, info, keyword, aka_title.phonetic_code, production_year, title, keyword
+SELECT info_type_id, movie_info.movie_id, info, keyword, aka_title.phonetic_code, production_year, title
  FROM imdb.aka_title
  JOIN imdb.movie_info ON movie_info.movie_id = aka_title.movie_id
  JOIN imdb.keyword ON imdb.aka_title.phonetic_code = keyword.phonetic_code
