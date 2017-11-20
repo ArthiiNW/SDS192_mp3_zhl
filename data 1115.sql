64a24e19b958d9b62b358b128a36e54d9fb02111
jhu70 <jhu70@bonny.smith.edu>
2017-11-20 11:40:02 -0500

    1120 change of Q1

--- data 1115.sql
+++ data 1115.sql
@@ -1,5 +1,5 @@
-SELECT info_type_id, movie_info.movie_id, info, keyword, aka_title.phonetic_code, production_year, title
- FROM imdb.aka_title
- JOIN imdb.movie_info ON movie_info.movie_id = aka_title.movie_id
- JOIN imdb.keyword ON imdb.aka_title.phonetic_code = keyword.phonetic_code
+SELECT info_type_id, movie_info.movie_id, info, keyword, title.phonetic_code, production_year, title
+ FROM imdb.title
+ JOIN imdb.movie_info ON movie_info.movie_id = title.id
+ JOIN imdb.keyword ON imdb.title.phonetic_code = keyword.phonetic_code
  HAVING info_type_id = 3 AND info = 'Western';
